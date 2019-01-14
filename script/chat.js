d6b6a5635d093524578b25ff97e2b27377de5a4a
Ernest Pakers <pakers.ernest@gmail.com>
2019-01-14 23:35:39 +0200

    Chat context. Sidebar refactored.

---
+++ script/chat.js
@@ -0,0 +1,6 @@
+let menuButton = document.querySelector('.menu-button');
+let sidebar = document.querySelector('.sidebar');
+
+menuButton.addEventListener('click', function() {
+    menuButton.classList.toggle('toggle');
+})
