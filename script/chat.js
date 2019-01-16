4d201b5591048b86b04a1d67ebbd3988f4db6d59
Ernest Pakers <pakers.ernest@gmail.com>
2019-01-16 02:25:20 +0200

    Mobile menu and unread message indicator

--- script/chat.js
+++ script/chat.js
@@ -1,6 +1,7 @@
 let menuButton = document.querySelector('.menu-button');
-let sidebar = document.querySelector('.sidebar');
+let container = document.querySelector('.container');
 
 menuButton.addEventListener('click', function() {
     menuButton.classList.toggle('toggle');
+    container.classList.toggle('menu-open');
 })
