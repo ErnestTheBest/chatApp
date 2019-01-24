024d782dd18740029d37a53e9ca84f9cb72b22f0
Ernest Pakers <pakers.ernest@gmail.com>
2019-01-24 22:44:39 +0200

    Accoutn info update prepared.

--- script/profilePageError.js
+++ script/profilePageError.js
@@ -2,50 +2,52 @@ let passwordOne = document.getElementById('password');
 let passwordTwo = document.getElementById('confirm-password');
 let displayName = document.getElementById('display-name');
 
-document.getElementById('cancel').addEventListener('click', function(event) {
+document.getElementById('cancel').addEventListener('click', function (event) {
     event.preventDefault()
     window.location.replace('./chat.html');
 });
 
 document.addEventListener('submit', function (event) {
     event.preventDefault();
-    register();
+    updateProfile();
 });
 
 function valid() {
     createAndDisplayPopup('Good boy', '#82df1b');
-    inputs.forEach(e => e.classList.remove('error'));
+    passwordOne.classList.remove('error');
+    passwordTwo.classList.remove('error');
 }
 
 function notValid() {
-    createAndDisplayPopup('Mandatory values must be entered', 'linear-gradient(#f95062, #df251b)')
-    inputs.forEach(e => {
-        if (!e.value) {
-            e.classList.add('error');
-        } else {
-            e.classList.remove('error');
-        }
-    });
-}
-
-function passwordDoNotMatch() {
-    createAndDisplayPopup('Passwords do not match', 'linear-gradient(#f95062, #df251b)');
+    createAndDisplayPopup('Passwords do not match', 'linear-gradient(#f95062, #df251b)')
     passwordOne.classList.add('error');
     passwordTwo.classList.add('error');
 }
 
-function validateFieldsNotEmpty() {
-    let areValid = inputs.every(e => e.value);
-    areValid ? valid() : notValid();
-    return areValid;
+function checkPasswordsChanged() {
+    // If password field is no empty
+    // And if passwords are equal
+    if (passwordOne.value && arePasswordsEqual()) {
+        valid();
+        return true;
+    } else {
+        notValid();
+        return false;
+    }
 }
 
 function arePasswordsEqual() {
     return passwordOne.value === passwordTwo.value;
 }
 
-function register() {
-    if (validateFieldsNotEmpty()) {
-        arePasswordsEqual() ? valid() : passwordDoNotMatch();
+function fetchAccountUpdate() {
+    console.log('Account update is not implemented yet. Soon...')
+}
+
+function updateProfile() {
+    //1. Check if password fields are not empty and are equal.
+    if (checkPasswordsChanged()) {
+        //2. If 1 is true, then send data to server.
+        fetchAccountUpdate();
     }
 }
