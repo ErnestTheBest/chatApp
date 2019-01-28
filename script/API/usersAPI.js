f6239fa30a966ee20b40e104982c034e2f3766e8
Ernest Pakers <pakers.ernest@gmail.com>
2019-01-29 01:16:30 +0200

    WIP: Update user profile. Not working yet.

--- script/API/usersAPI.js
+++ script/API/usersAPI.js
@@ -12,6 +12,35 @@ function registerNewUser(userName, pass) {
     });
 }
 
+function updateUserProfile(userName, passs, userId, name = undefined, pass = undefined) {
+    if (!name && !pass) {
+        return new Error('No values were passed for user update.')
+    } 
+
+    let requestBody = new FormData();
+
+    if (name) {
+        requestBody.set('name', name);
+    }
+
+    if (pass) {
+        requestBody.set('password', pass);
+    }
+
+    return fetch(`${defaultUrl}/users/${userId}`, {
+        method: 'PUT',
+        headers: {
+            Authorization: `Basic ${btoa(`${userName}:${passs}`)}`,
+            'Content-Type': 'application/json',
+        },
+        body: requestBody
+    }).then(res => {
+        return res.json();
+    });
+}
+
+// JSON.stringify({ username: name, password: pass })
+
 function loginExistingUser(userName, pass) {
     return fetch(`${defaultUrl}/me`, {
         headers: {
