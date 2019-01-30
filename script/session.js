00ecb213931181cbb9ebff3a0fe4df254600c5b6
Ernest Pakers <pakers.ernest@gmail.com>
2019-01-31 01:04:10 +0200

    Fix infinite loop

--- script/session.js
+++ script/session.js
@@ -1,11 +1,12 @@
 function checkSession() {
+    let pathname = window.location.pathname;
     if (!window.sessionStorage.getItem('userId')) {
-        if (window.location.pathname === '/index.html' || window.location.pathname === '/register.html') {
+        if (pathname.includes('/index.html') || pathname.includes('/register.html')) {
 
         } else {
             window.location.replace('./index.html');
         }
-    } else if (window.location.pathname === '/index.html' || window.location.pathname === '/register.html') {
+    } else if (pathname.includes('/index.html') || pathname.includes('/register.html')) {
         window.location.replace('./chat.html');
     }
 }
