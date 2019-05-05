e95c5d0e0cd3d4ea2107c487e382d453f4b329d0
Ernest Pakers <pakers.ernest@gmail.com>
2019-05-05 17:36:06 +0300

    Messages API done

--- src/script/API/messagesAPI.js
+++ src/script/API/messagesAPI.js
@@ -1,24 +1,9 @@
-import { defaultUrl } from './endpoints'
+import { doRequest } from './fetchWrapper'
 
-export function sendMessage (messageText) {
-  return window.fetch(`${defaultUrl}/users/${window.sessionStorage.userId}/contacts/${window.sessionStorage.chatContextId}/messages`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Basic ${window.sessionStorage.credentials}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ message: messageText })
-  }).then(res => {
-    return res.json()
-  })
+export function sendMessage (message) {
+  return doRequest('POST', `users/${window.sessionStorage.userId}/contacts/${window.sessionStorage.chatContextId}/messages`, { message })
 }
 
 export function getMessages () {
-  return window.fetch(`${defaultUrl}/users/${window.sessionStorage.userId}/contacts/${window.sessionStorage.chatContextId}/messages`, {
-    headers: {
-      Authorization: `Basic ${window.sessionStorage.credentials}`
-    }
-  }).then(res => {
-    return res.json()
-  })
+  return doRequest('GET', `users/${window.sessionStorage.userId}/contacts/${window.sessionStorage.chatContextId}/messages`)
 }
