b23c48b0db7435dce0f48fea2f543a187c44bdac
Ernest Pakers <pakers.ernest@gmail.com>
2019-05-05 17:10:01 +0300

    Finished API refactoring.

--- src/script/API/usersAPI.js
+++ src/script/API/usersAPI.js
@@ -1,38 +1,16 @@
+import { doRequest } from './fetchWrapper'
 import { defaultUrl } from './endpoints'
 
 export function getAllUsersList () {
-  return window.fetch(`${defaultUrl}/users`, {
-    headers: {
-      Authorization: `Basic ${window.sessionStorage.credentials}`
-    }
-  }).then(res => {
-    return res.json()
-  })
+  return doRequest('GET', 'users')
 }
 
-export function registerNewUser (userName, pass) {
-  return window.fetch(`${defaultUrl}/users`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ username: userName, password: pass })
-  }).then(res => {
-    return res.json()
-  })
+export function registerNewUser (username, password) {
+  return doRequest('POST', 'users', { username, password })
 }
 
 export function updateUserProfile (name, password = undefined) {
-  return window.fetch(`${defaultUrl}/users/${window.sessionStorage.userId}`, {
-    method: 'PUT',
-    headers: {
-      Authorization: `Basic ${window.sessionStorage.credentials}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ name, password })
-  }).then(res => {
-    return res.ok
-  })
+  return doRequest('PUT', `users/${window.sessionStorage.userId}`, { name, password })
 }
 
 export function loginExistingUser (userName, pass) {
@@ -46,12 +24,5 @@ export function loginExistingUser (userName, pass) {
 }
 
 export function getLoggedInUserInfo () {
-  return window.fetch(`${defaultUrl}/me`, {
-    headers: {
-      Authorization: `Basic ${window.sessionStorage.credentials}`
-    }
-  }).then(res => {
-    console.log(res)
-    return res.json()
-  })
+  return doRequest('GET', 'me')
 }
